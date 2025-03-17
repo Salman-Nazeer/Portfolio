cf89d863a32d2ad07821e74160faad905d32d92f
Salman-Nazeer <nazeersalman716@gmail.com>
2025-03-17 01:14:02 -0500

    experience section done

--- src/constants/index.js
+++ src/constants/index.js
@@ -159,11 +159,12 @@ export const myProjects = [
         id: 5,
         name: 'Express JS',
         path: 'skills/Express.svg',
-      },{
+      },
+      {
         id: 6,
         name: 'MongoDB',
         path: 'skills/MongoDB.svg',
-      }
+      },
     ],
   },
   {
@@ -317,32 +318,32 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
 export const workExperiences = [
   {
     id: 1,
-    name: 'Framer',
-    pos: 'Lead Web Developer',
-    duration: '2022 - Present',
+    name: 'Markey Tech',
+    pos: 'Front-End Developer',
+    duration: 'July 2024 - January 2025',
     title:
-      'Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.',
-    icon: '/assets/framer.svg',
+      'Designed and implemented interactive web applications using React.js, JavaScript, CSS, and HTML, focusing on modular, reusable components and scalable architecture. Optimized UI/UX and ensured responsiveness across devices.',
+    icon: '/assets/job.png',
     animation: 'victory',
   },
   {
     id: 2,
-    name: 'Figma',
+    name: 'Freelance',
     pos: 'Web Developer',
-    duration: '2020 - 2022',
+    duration: '2023 - Present',
     title:
-      'Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.',
-    icon: '/assets/figma.svg',
+      'Developed web applications using the MERN stack, focusing on performance, responsiveness, and intuitive UI/UX. Integrated RESTful APIs and implemented state management with Redux and Context API.',
+    icon: '/assets/Freelance.png',
     animation: 'clapping',
   },
   {
     id: 3,
-    name: 'Notion',
-    pos: 'Junior Web Developer',
-    duration: '2019 - 2020',
+    name: 'Personal Projects',
+    pos: 'Full-Stack Developer',
+    duration: 'Ongoing',
     title:
-      'Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.',
-    icon: '/assets/notion.svg',
+      'Built and deployed multiple projects, including an orrery web app, an e-commerce platform, and a task management tool using React, Tailwind CSS, Node.js, and MongoDB. Focused on caching, deployment, and optimizing performance.',
+    icon: '/assets/personal-project.webp',
     animation: 'salute',
   },
 ];
